64dae72e6c5e9787f0ba6f601f46e9aa0ab452c8
Linda-sunshine <gonglin1988@gmail.com>
2017-03-26 15:11:29 -0400

    add incident analysis

--- src/mains/IncidentMain.java
+++ src/mains/IncidentMain.java
@@ -57,34 +57,70 @@ public class IncidentMain {
 		String gayExpTestArff = String.format("%s/%s/ArffData/gay_test_exp_%s_%d_demo_%b.arff", prefix, data, fv, k, demo);
 
 		/***Generate testing Arff files based on the selected features.***/
-		System.out.println(String.format("Start generating %s testing tweets....", type));
-		UserAnalyzer blackImp = new UserAnalyzer(tokenModel, classNumber, features, Ngram, lengthThreshold, false);
+		System.out.println("Start generating testing arffs...");
+		UserAnalyzer blackAnalyzer = new UserAnalyzer(tokenModel, classNumber, blackFeatures, Ngram, lengthThreshold, false);
+		UserAnalyzer gayAnalyzer = new UserAnalyzer(tokenModel, classNumber, gayFeatures, Ngram, lengthThreshold, false);
+
+		blackAnalyzer.loadUserDir(tweetTest, suffix);
+		blackAnalyzer.loadIAT(blackIAT);
+		blackAnalyzer.setFeatureValues("TFIDF", 2);
+		blackAnalyzer.generateArffData(blackImpTestArff, "imp", demo);
+		blackAnalyzer.generateArffData(blackExpTestArff, "exp", demo);
 		
+		gayAnalyzer.loadUserDir(tweetTest, suffix);
+		gayAnalyzer.loadIAT(gayIAT);
+		gayAnalyzer.setFeatureValues("TFIDF", 2);
+		gayAnalyzer.generateArffData(blackImpTestArff, "imp", demo);
+		gayAnalyzer.generateArffData(blackExpTestArff, "exp", demo);
+	
+		// load the weights.
+		BufferedReader blackImpReader = new BufferedReader(new FileReader(blackImpTestArff));
+		Instances blackImpTest = new Instances(blackImpReader);
+		blackImpTest.setClassIndex(blackImpTest.numAttributes() - 1);
+		IncidentPrediction blackImpPred = new IncidentPrediction();
+		blackImpPred.loadWeights(blackImpModel);
+						
+		BufferedReader blackExpReader = new BufferedReader(new FileReader(blackExpTestArff));
+		Instances blackExpTest = new Instances(blackExpReader);
+		blackExpTest.setClassIndex(blackExpTest.numAttributes() - 1);
+		IncidentPrediction blackExpPred = new IncidentPrediction();
+		blackExpPred.loadWeights(blackExpModel);
+
+		BufferedReader gayImpReader = new BufferedReader(new FileReader(gayImpTestArff));
+		Instances gayImpTest = new Instances(gayImpReader);
+		gayImpTest.setClassIndex(gayImpTest.numAttributes() - 1);
+		IncidentPrediction gayImpPred = new IncidentPrediction();
+		gayImpPred.loadWeights(gayImpModel);
+			
+		BufferedReader gayExpReader = new BufferedReader(new FileReader(gayExpTestArff));
+		Instances gayExpTest = new Instances(gayExpReader);
+		gayExpTest.setClassIndex(gayExpTest.numAttributes() - 1);
+		IncidentPrediction gayExpPred = new IncidentPrediction();
+		gayExpPred.loadWeights(gayExpModel);
+			
+		int ttlSize = 0;
+		double blackImpPy = 0, blackExpPy = 0, gayImpPy = 0, gayExpPy = 0; 
+		Instance blackImpIns, blackExpIns, gayImpIns, gayExpIns;
 		
-		
-		test_analyzer.loadUserDir(tweetTest, suffix);
-		test_analyzer.loadIAT(testIAT);
-		test_analyzer.setFeatureValues("TFIDF", 2);
-		test_analyzer.generateArffData(testImpFile, "imp", demo);
-		test_analyzer.generateArffData(testExpFile, "exp", demo);
-		
-		try{
-			BufferedReader testImpReader = new BufferedReader(new FileReader(testFile));
-			Instances impTest = new Instances(testImpReader);
-			impTest.setClassIndex(impTest.numAttributes() - 1);
-			IncidentPrediction inc = new IncidentPrediction();
-			// load the weights.
-			inc.loadWeights(modelFile);
-			Instance ins;
-			double trueY = 0, predY = 0; 
-			for(int i=0; i<test.size(); i++){
-				ins = test.get(i);
-				trueY = ins.value(test.numAttributes()-1);
-				predY = inc.classify(ins);
-				System.out.print(String.format("TrueY:%.4f, predY by lr:%.4f", trueY, predY));
+		if(blackImpTest.size() == blackExpTest.size() && blackExpTest.size() == gayImpTest.size() && gayImpTest.size() == gayExpTest.size()){
+			ttlSize = blackImpTest.size();
+			for(int i=0; i<ttlSize; i++){
+				blackImpIns = blackImpTest.get(i);
+				blackExpIns = blackExpTest.get(i);
+				gayImpIns = gayImpTest.get(i);
+				gayExpIns = gayExpTest.get(i);
+				
+				blackImpPy = blackImpPred.classify(blackImpIns);
+				blackExpPy = blackExpPred.classify(blackExpIns);
+				gayImpPy = gayImpPred.classify(gayImpIns);
+				gayExpPy = gayExpPred.classify(gayExpIns);
+				
+				System.out.print(String.format("BlackImp:(%.4f,%.4f)\tBlackExp(%.4f,%.4f)\tGayImp(%.4f,%.4f)\tGayExp(%.4f,%.4f)\n", 
+						blackImpIns.value(blackImpIns.numAttributes()-1), blackImpPy,
+						blackExpIns.value(blackExpIns.numAttributes()-1), blackExpPy,
+						gayImpIns.value(gayImpIns.numAttributes()-1), gayImpPy,
+						gayExpIns.value(gayExpIns.numAttributes()-1), gayExpPy));
 			}
-		}catch(IOException e){
-			e.printStackTrace();
 		}
 	}
 }
